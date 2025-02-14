e2a46c4540e0502a9b2361e2cc3d5b501f783f66
Kannon Howard <kbHoward@protonmail.com>
2025-02-14 09:17:22 -0500

    initial push

---
+++ file_reader.py
@@ -0,0 +1,30 @@
+import os
+
+def open_file(filename):
+    """Open contents of a file, return a buffer of content"""
+    contents = []
+    try:
+        with open(filename, "r") as file:
+            for lines in file:
+                contents.append(lines.rstrip())
+    except FileNotFoundError:
+        print("File Not Found")
+        pass
+    return contents
+
+line_buffer = 10 
+current_index = 0
+#contents = open_file("/home/kannonhoward/Code/Python/AC-Data/music.csv")
+contents = open_file("/home/kannonhoward/Code/Python/text_test.txt")
+
+while (current_index < len(contents)):
+    os.system('clear')
+    for i in range(line_buffer):
+        if (current_index >= len(contents)):
+            break
+        else:
+            print(f"{current_index+1} {contents[current_index]}")
+            current_index += 1
+    check = input("Press Enter to Continue or q o quit: ")
+    if check == 'q':
+        break
