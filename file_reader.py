1607b335897d9718a05cec2ae48c2a425dcba7e9
Kannon Howard <kbHoward@protonmail.com>
2025-02-16 17:04:29 -0500

    read_file -> get_file_text, restructure with buffer instead of whole file.

--- file_reader.py
+++ file_reader.py
@@ -1,38 +1,26 @@
 import os
 import sys
 
-def open_file(filename):
+buffer_size = 1024
+
+def get_file_text(filename):
     """Open contents of a file, return a buffer of content"""
-    contents = []
+    contents = ''
     try:
         with open(filename, "r") as file:
-            for lines in file:
-                contents.append(lines.rstrip())
+            while True:
+                buffer = file.read(buffer_size)
+                if not buffer:
+                    break
+                contents +=  buffer
     except FileNotFoundError:
         print("File Not Found")
         pass
     return contents
 
-def read_file(filepath):
-    line_buffer = 10 
-    current_index = 0
-    contents = open_file(filepath)
-
-    while (current_index < len(contents)):
-        os.system('clear')
-        for i in range(line_buffer):
-            if (current_index >= len(contents)):
-                break
-            else:
-                print(f"{current_index+1} {contents[current_index]}")
-                current_index += 1
-        user_input = input("Press Enter to Continue or q to quit: ")
-        if user_input == 'q':
-            break
-
 filepath = None
 if len(sys.argv) > 1:
     filepath = sys.argv[1] 
-    read_file(filepath)
+    get_file_text(filepath)
 else:
     print("No File Input\n")
